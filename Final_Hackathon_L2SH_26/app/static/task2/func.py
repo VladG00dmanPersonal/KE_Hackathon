f385b56386dc9eaec1b72a4235a7137e014bd65a
Косяк Данила Сергеевич <kosyak.ds@students.sch2.ru>
2026-05-28 11:57:28 +0000

    upd_task2

--- Final_Hackathon_L2SH_26/app/static/task2/func.py
+++ Final_Hackathon_L2SH_26/app/static/task2/func.py
@@ -1,7 +1,22 @@
-import pandas as pd
-import numpy as np 
+import csv
 
-dp = pd.read_csv('./moscow_results.csv')
-print(dp[5])
+spb_file = open('spb_results.csv', newline='', encoding='cp1251')
+reader_spb = csv.DictReader(spb_file, delimiter=';')
 
-# def get_school(full_name):
+moscow_file = open('moscow_results.csv', newline='', encoding='cp1251')
+reader_moscow = csv.DictReader(moscow_file, delimiter=';')
+
+
+def get_school(FULL_NAME):
+    surname, name, lastname = FULL_NAME.split()
+    print(name, surname, lastname)
+    for i in reader_spb:
+        if i['Участник'] == surname + ' ' + name:
+            return (i['Школа'], 'Санкт-Петербург')
+    for i in reader_moscow:
+        # print(surname + ' ' + name)
+        if i['Участник'] == surname + ' ' + name:
+            return (i['Школа'], 'Москва')
+    return (None, None)
+
+print(get_school('Чеботаренко Никита 1123'))
